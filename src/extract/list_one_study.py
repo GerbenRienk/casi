6c41eead6e0738236b0d6a31aa23a49135b756e5
Gerben Rienk <GerbenRienk@GMail.com>
2020-12-08 17:25:11 +0100

    get friendly name of study

--- src/extract/list_one_study.py
+++ src/extract/list_one_study.py
@@ -10,7 +10,9 @@ Copyright 2020 TrialDataSolutions
 from utils.dictfile import DictFile
 from utils.castor_api import CastorApi
 
-def list_studies():
+def list_study():
+    # manually supply the study-id
+    study_id = '2A72D9CC-06B5-0078-B089-A5456C7A7024'
     # read configuration file for client id and client secret and other parameters
     config=DictFile('casi.config').read()
     # make an instance of the api
@@ -19,11 +21,13 @@ def list_studies():
     api.sessions.get_access_token(verbose=False)
     
     #request the studies
-    #all_studies = api.studies.list(verbose=False, complete_output=False)
-    all_studies = api.studies.list(verbose=True, complete_output=False)
-    # display name and id
-    for one_study in all_studies:
-        print(one_study['name'], '\t', one_study['study_id'])
+    my_study = api.study.list(study_id, verbose=True)
+    print(my_study) 
+    # display name if we have that
+    if 'name' in my_study:
+        print(my_study['name'])
+    else:
+        print('could not find a name for %s' % study_id)
             
 if __name__ == '__main__':
-    list_studies()
+    list_study()
